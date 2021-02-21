d08e56b8adf109e615d5af540d1d0f39351dcba5
Roma Kokontsev <Romeo42@ukr.net>
2021-02-22 00:45:31 +0200

    добавляет модалки для всех кнопок

--- src/js/modal-product.js
+++ src/js/modal-product.js
@@ -11,4 +11,34 @@
     function toggleModal() {
       refs.modal.classList.toggle('hidden');
     }
+})();
+
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector('[coffee-modal-open]'),
+      closeModalBtn: document.querySelector('[coffee-modal-close]'),
+      modal: document.querySelector('[coffee-modal]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('hidden');
+    }
+})();
+
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector('[milkshakes-modal-open]'),
+      closeModalBtn: document.querySelector('[milkshakes-modal-close]'),
+      modal: document.querySelector('[milkshakes-modal]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('hidden');
+    }
   })();
