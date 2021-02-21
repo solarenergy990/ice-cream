45a8f380df1ce23224b366be949a21b3f05ca82d
Vladymyr Rzhanov <v.rzhanov@gmail.com>
2021-02-21 17:02:24 +0200

    Добавляет трансформацию кнопке, активную ссылку, якоря

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -2,13 +2,16 @@
   const refs = {
     openModalBtn: document.querySelector('[data-menu-open]'),
     closeModalBtn: document.querySelector('[data-menu-close]'),
+    closeModalBurger: document.querySelector('[data-burger-close]'),
     modal: document.querySelector('[data-menu]'),
   };
-
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
-
+  refs.closeModalBurger.addEventListener('click', toggleModal);
   function toggleModal() {
     refs.modal.classList.toggle('menu--is-hidden');
+  }
+  function removeModal() {
+    refs.modal.classList.remove('menu--is-hidden');
   }
 })();
