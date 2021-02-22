8a501396468458ef358ef3ff41170384c670f64b
Vladymyr Rzhanov <v.rzhanov@gmail.com>
2021-02-22 02:07:22 +0200

    Убирает лишнее

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -3,14 +3,12 @@
     openModalBtn: document.querySelector('[burger-menu-open]'),
     closeModalBtn: document.querySelector('[burger-menu-close]'),
     closeModalBurger: document.querySelector('[burger-menu-move]'),
-    // closeModalBurger: document.querySelector('[burger-menu-remove]'),
     modal: document.querySelector('[burger-menu]'),
     burger: document.querySelector('[burger-icon]')
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
-  // refs.closeModalBurger.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('menu--is-hidden');
