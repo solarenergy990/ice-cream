ece923dab1882256c13fa2b243dca32ff6ad625c
Vladymyr Rzhanov <v.rzhanov@gmail.com>
2021-02-24 00:22:15 +0200

    Навешивает модалку покупки на продукты

--- src/js/menu-modal.js
+++ src/js/menu-modal.js
@@ -2,13 +2,19 @@
   const refs = {
     openModalBtn: document.querySelector('[buy-modal-open]'),
     openModalBurger: document.querySelector('[buy-burger-open]'),
+    openModalCream: document.querySelector('[buy-cream-open]'),
+    openModalCoffee: document.querySelector('[buy-coffee-open]'),
+    openModalShakes: document.querySelector('[buy-milkshakes-open]'),
     closeModalBtn: document.querySelector('[buy-modal-close]'),
-    closeModalThanks: document.querySelector('[thanks-modal-close]'),
+    closeModalThanks: document.querySelector('[buy-modal-remove]'),
     modal: document.querySelector('[buy-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.openModalBurger.addEventListener('click', toggleModal);
+  refs.openModalCream.addEventListener('click', toggleModal);
+  refs.openModalCoffee.addEventListener('click', toggleModal);
+  refs.openModalShakes.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
   refs.closeModalThanks.addEventListener('click', toggleModal);
 
